f186fdcf57488ddebcd31e343b5ea34b06022e2e
JJJ <971622721@qq.com>
2022-05-18 23:21:59 +0800

    初始化文档

---
+++ cmake-build-debug/CMakeFiles/key_open.elf.dir/DependInfo.cmake
@@ -0,0 +1,77 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "E:/a-component/a-key-open/key_open/startup/startup_stm32f103xb.s" "E:/a-component/a-key-open/key_open/cmake-build-debug/CMakeFiles/key_open.elf.dir/startup/startup_stm32f103xb.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "E:/a-component/a-key-open/key_open/Core/Src/gpio.c" "E:/a-component/a-key-open/key_open/cmake-build-debug/CMakeFiles/key_open.elf.dir/Core/Src/gpio.c.obj"
+  "E:/a-component/a-key-open/key_open/Core/Src/main.c" "E:/a-component/a-key-open/key_open/cmake-build-debug/CMakeFiles/key_open.elf.dir/Core/Src/main.c.obj"
+  "E:/a-component/a-key-open/key_open/Core/Src/stm32f1xx_hal_msp.c" "E:/a-component/a-key-open/key_open/cmake-build-debug/CMakeFiles/key_open.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj"
+  "E:/a-component/a-key-open/key_open/Core/Src/stm32f1xx_it.c" "E:/a-component/a-key-open/key_open/cmake-build-debug/CMakeFiles/key_open.elf.dir/Core/Src/stm32f1xx_it.c.obj"
+  "E:/a-component/a-key-open/key_open/Core/Src/syscalls.c" "E:/a-component/a-key-open/key_open/cmake-build-debug/CMakeFiles/key_open.elf.dir/Core/Src/syscalls.c.obj"
+  "E:/a-component/a-key-open/key_open/Core/Src/system_stm32f1xx.c" "E:/a-component/a-key-open/key_open/cmake-build-debug/CMakeFiles/key_open.elf.dir/Core/Src/system_stm32f1xx.c.obj"
+  "E:/a-component/a-key-open/key_open/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "E:/a-component/a-key-open/key_open/cmake-build-debug/CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "E:/a-component/a-key-open/key_open/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "E:/a-component/a-key-open/key_open/cmake-build-debug/CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "E:/a-component/a-key-open/key_open/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "E:/a-component/a-key-open/key_open/cmake-build-debug/CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "E:/a-component/a-key-open/key_open/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "E:/a-component/a-key-open/key_open/cmake-build-debug/CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "E:/a-component/a-key-open/key_open/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "E:/a-component/a-key-open/key_open/cmake-build-debug/CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "E:/a-component/a-key-open/key_open/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "E:/a-component/a-key-open/key_open/cmake-build-debug/CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "E:/a-component/a-key-open/key_open/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "E:/a-component/a-key-open/key_open/cmake-build-debug/CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "E:/a-component/a-key-open/key_open/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "E:/a-component/a-key-open/key_open/cmake-build-debug/CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "E:/a-component/a-key-open/key_open/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "E:/a-component/a-key-open/key_open/cmake-build-debug/CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "E:/a-component/a-key-open/key_open/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "E:/a-component/a-key-open/key_open/cmake-build-debug/CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "E:/a-component/a-key-open/key_open/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "E:/a-component/a-key-open/key_open/cmake-build-debug/CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "E:/a-component/a-key-open/key_open/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "E:/a-component/a-key-open/key_open/cmake-build-debug/CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "E:/a-component/a-key-open/key_open/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "E:/a-component/a-key-open/key_open/cmake-build-debug/CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/key_open.elf.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/key_open.elf.dir/Core/Src/gpio.c.obj"
+  "CMakeFiles/key_open.elf.dir/Core/Src/main.c.obj"
+  "CMakeFiles/key_open.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj"
+  "CMakeFiles/key_open.elf.dir/Core/Src/stm32f1xx_it.c.obj"
+  "CMakeFiles/key_open.elf.dir/Core/Src/syscalls.c.obj"
+  "CMakeFiles/key_open.elf.dir/Core/Src/system_stm32f1xx.c.obj"
+  "CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "CMakeFiles/key_open.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "CMakeFiles/key_open.elf.dir/startup/startup_stm32f103xb.s.obj"
+  "key_open.elf"
+  "key_open.elf.manifest"
+  "key_open.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/key_open.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
